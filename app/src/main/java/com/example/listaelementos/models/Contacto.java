8ac1db66a54124b027d42cb744ae9b990d73c5da
Daniela Galleguillos Díaz <dangalledi@gmail.com>
2021-11-10 15:19:52 -0300

    ADD: proyecto clases ListaElementosAndroid

---
+++ app/src/main/java/com/example/listaelementos/models/Contacto.java
@@ -0,0 +1,50 @@
+package com.example.listaelementos.models;
+
+import java.io.Serializable;
+
+public class Contacto implements Serializable {
+
+    String nombre;
+    String paterno;
+    String materno;
+    String telefono;
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPaterno() {
+        return paterno;
+    }
+
+    public void setPaterno(String paterno) {
+        this.paterno = paterno;
+    }
+
+    public String getMaterno() {
+        return materno;
+    }
+
+    public void setMaterno(String materno) {
+        this.materno = materno;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+
+    @Override
+    public String toString() {
+        return nombre + " " + paterno + " " + materno ;
+    }
+}
